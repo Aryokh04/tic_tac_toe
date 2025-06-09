eceda1c81638eec4ddf0bfac18a6b629aafcdd56
Aryokh04 <143172110+Aryokh04@users.noreply.github.com>
2025-06-09 16:40:24 +0200

    programmed the board

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -7,11 +7,13 @@ class TicTacToeBoard(tk.Tk):
         super().__init__()
         self.title("Tic Tac Toe")
         self._cells = {}
+        self._create_board_display()
+        self._create_board_grid()
 
     def _create_board_display(self):
         display_frame = tk.Frame(master=self)
         display_frame.pack(fill=tk.X)
-        self.display = tk.label(
+        self.display = tk.Label(
             master=display_frame,
             text="ready?",
             font=font.Font(size=30, weight="bold"),
@@ -19,5 +21,33 @@ class TicTacToeBoard(tk.Tk):
         self.display.pack()
 
     def _create_board_grid(self):
-        grid_frame 
+        grid_frame = tk.Frame(master=self)
+        grid_frame.pack()
+        for row in range(3):
+            self.rowconfigure(row, weight=1, minsize=50)
+            self.columnconfigure(row, weight=1, minsize=75)
+            for col in range(3):
+                button = tk.Button(
+                    master=grid_frame,
+                    text="",
+                    font=font.Font(size=36, weight="bold"),
+                    fg="black",
+                    width=3,
+                    height=2,
+                    highlightbackground="lightblue",
+                )
+                self._cells[button] = (row, col)
+                button.grid(
+                    row=row,
+                    column=col,
+                    padx=5,
+                    pady=5,
+                    sticky="nsew"
+                )
+def main():
+    board = TicTacToeBoard()
+    board.mainloop()
+
+if __name__ == "__main__":
+    main()
 
