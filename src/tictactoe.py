36f55f5c44294e26d7a8f00fd39d2575bd586632
Aryokh04 <143172110+Aryokh04@users.noreply.github.com>
2025-06-09 19:30:44 +0200

    creating reset_board function

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -12,6 +12,8 @@ class TicTacToeBoard(tk.Tk):
         self._create_board_display()
         self._create_board_grid()
         self._player_turn = True  # True = player X, False = player O (AI)
+        self._board = [["" for _ in range(3)] for _ in range(3)]   # Empty board 
+
 
     def _create_board_display(self):
         display_frame = tk.Frame(master=self)
@@ -22,6 +24,7 @@ class TicTacToeBoard(tk.Tk):
             font=font.Font(size=30, weight="bold"),
         )
         self.display.pack()
+
 
     def _create_board_grid(self):
         grid_frame = tk.Frame(master=self)
@@ -70,8 +73,11 @@ class TicTacToeBoard(tk.Tk):
         return None
     
     def is_board_full(self):
-
-
+        for row in self._board:
+            for cell in row:
+                if cell == "":
+                    return False
+        return True
 
 
     def _handle_click(self, row, col, button):
@@ -87,10 +93,18 @@ class TicTacToeBoard(tk.Tk):
                 return 
             elif self.is_board_full():
                 self.display.config(text="It Is A Tie!")
-                self._game_over = True 
+                self.reset_board
                 return
             
         self.after(300, self.make_ai_move)
+    
+    def reset_board(self):
+        for button in self._cells:
+            button.config(text="", state=tk.NORMAL)
+        self._board = [["" for _ in range(3)] for _ in range(3)]
+        self._player_turn = True 
+        self._game_over = False 
+        self.display.config(text="New Game! X goes first")
     
    
 def main():
